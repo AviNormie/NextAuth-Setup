b272011984d2e64677ab65b8ebb80349fbf0402c
Avi Srivastava <srivastavaavi26@gmail.com>
2025-03-14 15:54:35 +0530

    add: Github Login

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"], // ✅ Allow Google profile images
+    domains: ["lh3.googleusercontent.com","avatars.githubusercontent.com"],
   },
 };
 
